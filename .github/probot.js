0a880071d85a31296961ece28b2fc70c5edc9a22
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2019-10-05 21:03:24 -0400

    update probot.js

--- .github/probot.js
+++ .github/probot.js
@@ -1,24 +1,8 @@
 on('issues.opened')
   .comment(`
-Hey  @{{ sender.login }}!
+    Hello @{{ sender.login }}. Thanks for inviting me to your project.
+    Read more about [all the things I can help you with][config]. I can't
+    wait to get started!
 
-This is just a test.
+    [config]: https://github.com/bkeepers/workflow/blob/master/docs/configuration.md
   `);
-
-/**
- * Minimize comment length
- */
-/*
-on('issue_comment.created')
-  .filter(context => context.payload.comment.body
-    .trim()
-    .replace(/\s+/gi, ' ')
-    .split(' ')
-    .length > 200
-  )
-  .comment(`
-Thanks for your comment, @{{ sender.login }}!
-Your reply is very long, and even if for me it takes less than a second to
-read it, summarizing it a bit will help old-fashioned humans. :smiley:
-  `);
-  */
